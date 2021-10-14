bd3c5cc208e32bca2d351baf517dd9581f4f5692
infzer0 <johnortizo@outlook.com>
2021-10-13 19:44:50 -0500

    MySQL Ejercicio: 211 2/2 Recuperar Registros de la Tabla peliculas con los Operadores LIKE y NOT LIKE
    Recuperar Registros de la Tabla peliculas con los Operadores LIKE y NOT LIKE con el lenguaje de consulta estructurado SQL.

--- parte001/ex211_peliculas_consultas_registros_con_operador_like.sql
+++ parte001/ex211_peliculas_consultas_registros_con_operador_like.sql
@@ -4,33 +4,33 @@ DROP TABLE IF EXISTS peliculas;
 -- 2. Créela con la siguiente estructura:
 CREATE TABLE peliculas (
     codigo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(30) NOT NULL,
-    actor VARCHAR(20) NOT NULL,
-    duracion TINYINT UNSIGNED NOT NULL
+    titulo VARCHAR(30),
+    actor VARCHAR(20),
+    duracion TINYINT UNSIGNED
 );
 
 -- 3. Visualice la estructura de la tabla "peliculas":
 DESCRIBE peliculas;
 
 -- 4. Ingrese los siguientes registros:
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Mision imposible','Tom Cruise',120);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Harry Potter y la piedra filosofal','Daniel R.',180);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Harry Potter y la camara secreta','Daniel R.',190);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Mision imposible 2','Tom Cruise',120);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Mujer bonita','Richard Gere',120);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Tootsie','D. Hoffman',90);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Un oso rojo',null,100);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Elsa y Fred','China Zorrilla',110);
- INSERT INTO peliculas (titulo,actor,duracion)
-  VALUES('Mrs. Johns','Richard Gere',180);
+ insert into peliculas (titulo,actor,duracion)
+  values('Mision imposible','Tom Cruise',120);
+ insert into peliculas (titulo,actor,duracion)
+  values('Harry Potter y la piedra filosofal','Daniel R.',180);
+ insert into peliculas (titulo,actor,duracion)
+  values('Harry Potter y la camara secreta','Daniel R.',190);
+ insert into peliculas (titulo,actor,duracion)
+  values('Mision imposible 2','Tom Cruise',120);
+ insert into peliculas (titulo,actor,duracion)
+  values('Mujer bonita','Richard Gere',120);
+ insert into peliculas (titulo,actor,duracion)
+  values('Tootsie','D. Hoffman',90);
+ insert into peliculas (titulo,actor,duracion)
+  values('Un oso rojo',null,100);
+ insert into peliculas (titulo,actor,duracion)
+  values('Elsa y Fred','China Zorrilla',110);
+ insert into peliculas (titulo,actor,duracion)
+  values('Mrs. Johns','Richard Gere',180);
 
 SELECT * FROM peliculas;
 
@@ -47,3 +47,21 @@ SELECT * FROM peliculas WHERE actor LIKE '%Gere%';
 -- campo "actor" (2 registros):
 SELECT * FROM peliculas WHERE titulo NOT LIKE '%y%' AND actor LIKE '%ch%';
 
+-- 8. Seleccione las películas que comiencen con "M" y cuya duración sea menor a 150 (3 registros):
+SELECT * FROM peliculas WHERE titulo NOT LIKE 'm%' AND duracion < 150;
+
+-- 9. Cambie el valor de la duración a 100 en las películas en las cuales el campo "actor" comience 
+-- con "D":
+UPDATE peliculas SET duracion = 100 WHERE actor LIKE 'D%';
+
+SELECT * FROM peliculas;
+
+-- 10. Recupere los registros que cumplan la condición del punto anterior, para verificar el cambio de 
+-- la duración:
+SELECT * FROM peliculas WHERE actor LIKE 'D%';
+
+-- 11. Vea si existen películas con títulos nulos:
+SELECT * FROM peliculas WHERE titulo LIKE NULL;
+
+-- 12. Vea si existen películas con valor nulo en el campo "actor"
+SELECT * FROM peliculas WHERE actor LIKE NULL;
