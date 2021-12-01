b15df284bf030a0c42a9fb667160c79c11255f41
infzer0 <johnortizo@outlook.com>
2021-12-01 11:53:26 -0500

    MySQL Ejercicio: 225 Obtener Valor Mínimo, Máximo, Promedio y Suma de los Registros de la Tabla libros
    Obtener Valor Mínimo, Máximo, Promedio y Suma de los Registros de la Tabla libros con el lenguaje de consulta estructurado SQL.

---
+++ parte001/ex225_libros_demo_uso_funciones_min_max_avg_sum.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS libros;
+
+CREATE TABLE libros(
+  codigo INT UNSIGNED AUTO_INCREMENT,
+  titulo VARCHAR(40) NOT NULL,
+  autor VARCHAR(30),
+  editorial VARCHAR(15),
+  precio DECIMAL(5,2) UNSIGNED,
+  cantidad MEDIUMINT UNSIGNED,
+  PRIMARY KEY(codigo)
+ );
+
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('El aleph','Borges','Planeta',15,100);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Martin Fierro','Jose Hernandez','Emece',22.20,200);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Antologia poetica','J.L. Borges','Planeta',40,150);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Aprenda PHP','Mario Molina','Emece',18.20,200);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Cervantes y el quijote','Bioy Casares- J.L. Borges','Paidos',36.40,100);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Manual de PHP', 'J.C. Paez', 'Paidos',30.80,120);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Harry Potter y la piedra filosofal','J.K. Rowling','Paidos',45.00,50);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Harry Potter y la camara secreta','J.K. Rowling','Paidos',46.00,100);
+INSERT INTO libros (titulo,autor,editorial,precio,cantidad)
+  VALUES('Alicia en el pais de las maravillas','Lewis Carroll','Paidos', NULL,200);
+
+SELECT SUM(cantidad) FROM libros;
+
+SELECT SUM(cantidad) FROM libros
+  WHERE editorial ='Planeta';
+
+SELECT MAX(precio) FROM libros;
+
+SELECT * FROM libros
+  ORDER BY precio DESC;
+
+SELECT min(precio) FROM libros
+  WHERE autor LIKE '%Rowling%';
+
+SELECT * FROM libros
+  WHERE autor LIKE '%Rowling%'
+  ORDER BY 5;
+
+SELECT avg(precio) FROM libros
+  WHERE titulo LIKE '%PHP%';
+
+SELECT * FROM libros
+  WHERE titulo LIKE '%PHP%';
