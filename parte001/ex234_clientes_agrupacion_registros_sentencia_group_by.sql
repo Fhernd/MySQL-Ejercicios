9deec56371b88b3a8bdae99b52b47900fe3b61e1
infzer0 <johnortizo@outlook.com>
2021-12-02 09:05:53 -0500

    MySQL Ejercicio: 234 Utilizar la Tabla clientes para Aplicar Operaciones de Agrupación GROUP BY
    Utilizar la Tabla clientes para Aplicar Operaciones de Agrupación GROUP BY con el lenguaje de consulta estructurado SQL.

---
+++ parte001/ex234_clientes_agrupacion_registros_sentencia_group_by.sql
@@ -0,0 +1,44 @@
+-- 1. Elimine la tabla "clientes", si existe:
+DROP TABLE IF EXISTS clientes;
+
+-- 2. Créela con la siguiente estructura:
+CREATE TABLE clientes (
+  codigo INT UNSIGNED AUTO_INCREMENT,
+  nombre VARCHAR(30) NOT NULL,
+  domicilio VARCHAR(30),
+  ciudad VARCHAR(20),
+  provincia VARCHAR (20),
+  telefono VARCHAR(11),
+  PRIMARY KEY(codigo)
+ );
+
+-- 3. Ingrese algunos registros:
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Lopez Marcos', 'Colon 111', 'Córdoba','Cordoba','null');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Perez Ana', 'San Martin 222', 'Cruz del Eje','Cordoba','4578585');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Garcia Juan', 'Rivadavia 333', 'Villa Maria','Cordoba','4578445');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Perez Luis', 'Sarmiento 444', 'Rosario','Santa Fe',NULL);
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono) 
+  VALUES ('Pereyra Lucas', 'San Martin 555', 'Cruz del Eje','Cordoba','4253685');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Gomez Ines', 'San Martin 666', 'Santa Fe','Santa Fe','0345252525');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Torres Fabiola', 'Alem 777', 'Villa del Rosario','Cordoba','4554455');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Lopez Carlos', 'Irigoyen 888', 'Cruz del Eje','Cordoba',NULL);
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Ramos Betina', 'San Martin 999', 'Cordoba','Cordoba','4223366');
+INSERT INTO clientes (nombre,domicilio,ciudad,provincia,telefono)
+  VALUES ('Lopez Lucas', 'San Martin 1010', 'Posadas','Misiones','0457858745');
+
+-- 4. Obtenga el total de los registros (10):
+SELECT COUNT(*) FROM clientes;
+
+-- 5. Obtenga el total de los registros que no tienen valor nulo en los teléfonos (8):
+SELECT COUNT(telefono) FROM clientes;
+
+-- 6. Obtenga la cantidad de clientes agrupados por ciudad y provincia, ordenados por provincia:
+SELECT ciudad, provincia, COUNT(*) FROM clientes GROUP BY ciudad, provincia;
