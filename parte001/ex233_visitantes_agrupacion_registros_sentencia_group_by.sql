7f8492ee3066058531eacadf547ea55879921ace
infzer0 <johnortizo@outlook.com>
2021-12-02 08:54:34 -0500

    MySQL Ejercicio: 233 Agrupación de Registros de la Tabla visitantes Usando la Sentencia GROUP BY
    Agrupación de Registros de la Tabla visitantes Usando la Sentencia GROUP BY con el lenguaje de consulta estructurado SQL.

---
+++ parte001/ex233_visitantes_agrupacion_registros_sentencia_group_by.sql
@@ -0,0 +1,66 @@
+DROP TABLE IF EXISTS visitantes;
+
+-- 2. Créela con la siguiente estructura:
+ CREATE TABLE visitantes(
+  nombre VARCHAR(30),
+  edad TINYINT UNSIGNED,
+  sexo CHAR(1),
+  domicilio VARCHAR(30),
+  ciudad VARCHAR(20),
+  telefono VARCHAR(11),
+  montocompra DECIMAL (6,2) UNSIGNED
+ );
+
+-- 3. Ingrese algunos registros:
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Susana Molina', 28,'f','Colon 123','Cordoba',NULL,45.50); 
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Marcela Mercado',36,'f','Avellaneda 345','Cordoba','4545454',0);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Alberto Garcia',35,'m','Gral. Paz 123','Alta Gracia','03547123456',25); 
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Teresa Garcia',33,'f','Gral. Paz 123','Alta Gracia','03547123456',0);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Roberto Perez',45,'m','Urquiza 335','Cordoba','4123456',33.20);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Marina Torres',22,'f','Colon 222','Villa Dolores','03544112233',25);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Julieta Gomez',24,'f','San Martin 333','Alta Gracia','03547121212',53.50);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Roxana Lopez',20,'f','Triunvirato 345','Alta Gracia',NULL,0);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Liliana Garcia',50,'f','Paso 999','Cordoba','4588778',48);
+ INSERT INTO visitantes (nombre,edad, sexo,domicilio,ciudad,telefono,montocompra)
+  VALUES ('Juan Torres',43,'m','Sarmiento 876','Cordoba','4988778',15.30);
+
+SELECT ciudad, COUNT(*)
+  FROM visitantes
+  GROUP BY ciudad;
+
+SELECT ciudad, COUNT(telefono)
+ FROM visitantes
+  GROUP BY ciudad;
+
+SELECT sexo, SUM(montocompra) FROM visitantes
+  GROUP BY sexo;
+
+SELECT sexo, MAX(montocompra) FROM visitantes
+  GROUP BY sexo;
+
+SELECT sexo, MIN(montocompra) FROM visitantes
+  GROUP BY sexo;
+
+SELECT sexo, MAX(montocompra),
+  MIN(montocompra)
+  FROM visitantes
+  GROUP BY sexo;
+
+SELECT ciudad, AVG(montocompra) FROM visitantes
+  GROUP BY ciudad;
+
+SELECT ciudad, sexo, COUNT(*) FROM visitantes
+  GROUP BY ciudad,sexo;
+
+SELECT ciudad, COUNT(*) FROM visitantes
+  WHERE ciudad<>'Cordoba'
+  GROUP BY ciudad;
