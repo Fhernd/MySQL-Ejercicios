05bb513acee4ce416ba274ac5fb4ee89e5032eb2
infzer0 <johnortizo@outlook.com>
2021-12-04 16:42:50 -0500

    MySQL Ejercicio: 248 Usar la Claúsula DISTINCT para Contar Registros Distintos de la Tabla cursos
    Usar la Claúsula DISTINCT para Contar Registros Distintos de la Tabla cursos con el lenguaje de consulta estructurado SQL.

--- parte001/ex248_curso_distinct_clausula_registros_unicos.sql
+++ parte001/ex248_curso_distinct_clausula_registros_unicos.sql
@@ -13,28 +13,28 @@ DROP TABLE IF EXISTS cursos;
  );
 
 -- 3. Ingrese los siguientes registros:
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('PHP básico','AM',10,'2006-08-07',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('PHP básico','PM',default,'2006-08-14',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('PHP básico','AM',default,'2006-08-05',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('PHP avanzado','AM',20,'2006-08-01',350);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('JavaScript básico','PM',15,'2006-09-11',150);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('Paginas web','PM',15,'2006-08-08',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('Paginas web','AM',15,'2006-08-12',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('Paginas web','AM',15,'2006-08-21',200);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('HTML avanzado','AM',20,'2006-09-18',180);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('HTML avanzado','PM',20,'2006-09-25',180);
- insert into cursos (tema,horario,clases,fechainicio,costo)
-  values('JavaScript avanzado','PM',25,'2006-09-18',150);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('PHP básico','AM',10,'2006-08-07',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('PHP básico','PM',DEFAULT,'2006-08-14',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('PHP básico','AM',DEFAULT,'2006-08-05',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('PHP avanzado','AM',20,'2006-08-01',350);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('JavaScript básico','PM',15,'2006-09-11',150);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('Paginas web','PM',15,'2006-08-08',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('Paginas web','AM',15,'2006-08-12',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('Paginas web','AM',15,'2006-08-21',200);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('HTML avanzado','AM',20,'2006-09-18',180);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('HTML avanzado','PM',20,'2006-09-25',180);
+ INSERT INTO cursos (tema,horario,clases,fechainicio,costo)
+  VALUES ('JavaScript avanzado','PM',25,'2006-09-18',150);
 
 -- 4. Obtenga la lista de temas de los cursos sin repetición:
 SELECT DISTINCT tema FROM cursos;
